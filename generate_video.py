7e1f2427cf4b4211ac08a242659116297a04edf2
Ludovica Gonella <ludovica.gonella@gmail.com>
2019-06-20 20:22:36 -0700

    add print to debug

--- generate_video.py
+++ generate_video.py
@@ -63,6 +63,7 @@ model = create_model(opt)
 
 frames_count = 1
 for f in tqdm(frames):
+    print(f'CUR FRAME: {f}')
     current_frame = video_utils.im2tensor(Image.open(f))
     next_frame = video_utils.next_frame_prediction(model, current_frame)
 
